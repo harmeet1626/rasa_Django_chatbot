921a3fd74a8f8fc7a00c807c07be89167e98e125
ritesh <riteshp.softprodigy@gmail.com>
2023-12-11 16:12:34 +0530

    chat history api

--- rasa_chat_app/serializers.py
+++ rasa_chat_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from rasa_chat_app.models import Tickets
+from rasa_chat_app.models import Tickets,Chats
 
 class UploadDocumentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tickets
         fields = ["ext_id","document"]
+
+class ChatsListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chats
+        fields = "__all__"
+
